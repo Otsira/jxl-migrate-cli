6301736190f12e8392226adc82d53a049a71a756
Carlos Albornoz <albornozmcea@gmail.com>
2023-09-20 15:04:48 -0300

    listfiles

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import typer
+from typing_extensions import Annotated
+import os
+
+app = typer.Typer()
 
 
-def main(name: str):
-    print(f"Hello {name}")
+@app.command()
+def main(input_dir: str):
+    for root, _, files in os.walk(input_dir):
+        for filename in files:
+            print(os.path.join(root, filename))
+
+    pass
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
